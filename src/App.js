f9814de9d23c301d75486503d8acf0184ce859dc
Natanael Matos <natanbmatos@gmail.com>
2022-02-09 03:16:05 -0300

    fix mobile first positioning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import LandingPage from "./components/LandingPage";
-import "./App.css";
 import Home from "./components/Home";
+import "./App.css";
 
 function App() {
 
